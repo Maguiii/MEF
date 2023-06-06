c2ca43aae75e8123be4af47bda3c1c17edc76ffa
Maguiii <magaliarebull@gmail.com>
2023-06-06 12:52:09 -0300

    Implementacion con el encendido de un led

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,14 @@
 #include <Arduino.h>
 #include <TimerOne.h>
-/*Este codigo es la Maquina de estados finitos que se va a usar para los pulsadores*/
+
+/*Este codigo es la Maquina de estados finitos que se va a usar para los pulsadores
+  En este caso se esta implementando con un pulsador y un led 
+  A este codigo le falta correccion 
+*/
 #define FALSE 0
 #define TRUE 1
 #define puls 10
+#define led 9
 
 volatile int t = 0;
 volatile int estado = 1;
@@ -51,6 +56,12 @@ void loop(){
     case 1: state1(); break;
     case 2: state2(); break;
     case 3: state3(); break;
+  }
+  if(flagPulso == TRUE){
+    digitalWrite(led, HIGH);
+  }
+  else{
+    digitalWrite(led, LOW);
   }
 }
 
